fffa6b7eb073e225ea4ba0f4a5e301eeb3e711ef
b221b <titarenkomiroslav61@gmail.com>
2025-04-15 15:48:24 +0300

    fixed some issues

--- views/site/tableData.php
+++ views/site/tableData.php
@@ -13,6 +13,39 @@ $tableNamesMap = [
     'trainers' => 'Тренеры',
 ];
 
+// Массив соответствий столбцов и их русских названий
+$columnLabels = [
+    'kind_of_sport' => [
+        'name' => 'Название',
+    ],
+    'organisations' => [
+        'full_name' => 'Название организатора',
+    ],
+    'prizewinner' => [
+        'prize_place' => 'Призовое место',
+        'reward' => 'Награда',
+    ],
+    'sports_club' => [
+        'name' => 'Название клуба',
+    ],
+    'sportsman' => [
+        'name' => 'Имя',
+        'discharge' => 'Разряд',
+        'id_sports_club' => 'Номер спортивного клуба',
+    ],
+    'structure' => [
+        'name' => 'Название сооружения',
+        'type' => 'Тип',
+    ],
+    'trainers' => [
+        'name' => 'ФИО тренера',
+        'id_kind_of_sport' => 'Вид спорта',
+    ],
+];
+
+$this->title = ($tableNamesMap[$tableName] ?? $tableName);
+$this->params['breadcrumbs'][] = $this->title;
+
 $this->title = 'Данные таблицы: ' . ($tableNamesMap[$tableName] ?? $tableName);
 ?>
 
@@ -20,5 +53,20 @@ $this->title = 'Данные таблицы: ' . ($tableNamesMap[$tableName] ?? 
 
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
-    'columns' => array_keys(Yii::$app->db->getTableSchema($tableName)->columns),
+    'columns' => array_map(
+        function ($column) use ($tableName, $columnLabels) {
+            return [
+                'attribute' => $column,  // Поле в БД
+                'label' => $columnLabels[$tableName][$column] ?? $column,  // Русское название (если есть)
+            ];
+        },
+        array_filter(
+            array_keys(Yii::$app->db->getTableSchema($tableName)->columns),
+            function ($column) {
+                return $column !== 'id'
+                    && $column !== 'id_sports_club'
+                    && $column !== 'id_kind_of_sport';
+            }
+        )
+    ),
 ]) ?>
