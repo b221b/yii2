80cef44ef31550846c85672a925a438391a03cb2
b221b <titarenkomiroslav61@gmail.com>
2025-05-14 12:19:13 +0300

    made more beautiful news page

--- views/newss/_item.php
+++ views/newss/_item.php
@@ -1,27 +1,55 @@
-<?php
-
-use yii\helpers\Html;
-use yii\helpers\Url;
-?>
-
 <div class="news-item">
-    <h3><?= Html::a(Html::encode($model->name), ['view', 'id' => $model->id]) ?></h3>
+    <div class="news-image">
+        <?php
 
-    <div class="meta">
-        <span class="date"><?= Yii::$app->formatter->asDate($model->event_date) ?></span>
-        <span class="sport"><?= Html::encode($model->sport->name ?? '') ?></span>
+        use yii\helpers\Html;
+
+        // Определяем изображение в зависимости от вида спорта
+        $sportImage = 'https://shkolainternatzmeinogorskaya-r22.gosweb.gosuslugi.ru/netcat_files/48/2472/1671737146_kartinkin_net_p_legkaya_atletika_kartinki_dlya_detei_krasi_25.jpg'; // изображение по умолчанию
+
+        if ($model->sport) {
+            switch (strtolower($model->sport->name)) {
+                case 'Футбол':
+                    $sportImage = 'https://www.sestrikcup.ru/photos/2013/99/max/zp1370500.jpg';
+                    break;
+                case 'Баскетбол':
+                    $sportImage = 'https://sportishka.com/uploads/posts/2021-11/1637335306_92-sportishka-com-p-igra-basketbol-komandnii-sport-foto-100.jpg';
+                    break;
+                case 'Волейбол':
+                    $sportImage = 'https://avatars.mds.yandex.net/i?id=cabc30b40b3a5e0cc05d9c5a44a29772_l-5207283-images-thumbs&n=13';
+                    break;
+                case 'легкая атлетика':
+                    $sportImage = 'https://shkolainternatzmeinogorskaya-r22.gosweb.gosuslugi.ru/netcat_files/48/2472/1671737146_kartinkin_net_p_legkaya_atletika_kartinki_dlya_detei_krasi_25.jpg';
+                    break;
+                    // добавьте другие виды спорта по необходимости
+            }
+        }
+        ?>
+        <img src="<?= $sportImage ?>" alt="<?= Html::encode($model->sport->name ?? 'Новости спорта') ?>">
     </div>
 
-    <div class="description">
-        <?= Html::encode($model->description) ?>
-    </div>
+    <div class="news-content">
+        <div class="news-meta">
+            <span class="news-date"><?= Yii::$app->formatter->asDate($model->event_date) ?></span>
+            <?php if ($model->sport): ?>
+                <span class="news-sport"><?= Html::encode($model->sport->name) ?></span>
+            <?php endif; ?>
+        </div>
 
-    <div class="organisations">
-        <strong>Организаторы:</strong>
-        <?php foreach ($model->organisations as $org): ?>
-            <span><?= Html::encode($org->full_name) ?></span>
-        <?php endforeach; ?>
-    </div>
+        <h3 class="news-title"><?= Html::a(Html::encode($model->name), ['view', 'id' => $model->id]) ?></h3>
 
-    <?= Html::a('Подробнее', ['view', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <div class="news-excerpt">
+            <?= Html::encode(mb_substr($model->description, 0, 150) . '...') ?>
+        </div>
+
+        <?php if (!empty($model->organisations)): ?>
+            <div class="news-organizers">
+                <?php foreach ($model->organisations as $org): ?>
+                    <span class="organizer-tag"><?= Html::encode($org->full_name) ?></span>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+
+        <?= Html::a('Читать далее', ['view', 'id' => $model->id], ['class' => 'read-more']) ?>
+    </div>
 </div>
--- views/newss/index.php
+++ views/newss/index.php
@@ -2,32 +2,65 @@
 
 use yii\helpers\Html;
 use yii\widgets\ListView;
-use yii\widgets\LinkPager;
 
-$this->title = 'Новости спортивных событий';
+$this->title = 'Новости';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="news-index">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="sport-filter">
-        <h4>Фильтр по виду спорта</h4>
-        <ul>
-            <li><?= Html::a('Все', ['index']) ?></li>
-            <?php foreach ($sports as $sport): ?>
-                <li><?= Html::a($sport->name, ['index', 'sport' => $sport->id]) ?></li>
-            <?php endforeach; ?>
-        </ul>
+<div class="news-page">
+    <!-- Hero-баннер -->
+    <div class="news-header">
+        <div class="container">
+            <h1><?= Html::encode($this->title) ?></h1>
+            <p class="lead">Самые свежие новости из мира спорта</p>
+        </div>
     </div>
 
-    <?= \app\widgets\LastNewsWidget::widget() ?>
+    <div class="container news-container">
+        <!-- Основной контент -->
+        <div class="row">
+            <!-- Боковая панель -->
+            <aside class="col-lg-3 sidebar">
+                <!-- Виджет последних новостей -->
+                <div class="sidebar-widget">
+                    <h3 class="widget-title">Последние новости</h3>
+                    <?= \app\widgets\LastNewsWidget::widget() ?>
+                </div>
 
-    <?= ListView::widget([
-        'dataProvider' => $dataProvider,
-        'itemView' => '_item',
-        'layout' => "{items}\n{pager}",
-        'options' => ['class' => 'news-list'],
-        'itemOptions' => ['class' => 'news-item'],
-    ]); ?>
+                <!-- Фильтр по видам спорта -->
+                <div class="sidebar-widget sport-filter">
+                    <h3 class="widget-title">Виды спорта</h3>
+                    <ul class="sport-list">
+                        <li><?= Html::a('Все новости', ['index'], ['class' => 'active']) ?></li>
+                        <?php foreach ($sports as $sport): ?>
+                            <li><?= Html::a($sport->name, ['index', 'sport' => $sport->id]) ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+
+                <!-- Баннер или реклама -->
+                <div class="sidebar-widget promo-banner">
+                    <img src="https://photosota.club/uploads/posts/2023-07/1689502880_photosota-club-p-beg-shkolnikov-21.jpg" alt="Спортивные события" class="img-fluid">
+                    <div class="promo-text">Подпишитесь на наши новости</div>
+                </div>
+            </aside>
+
+            <!-- Основная новостная лента -->
+            <main class="col-lg-9 news-feed">
+                <?= ListView::widget([
+                    'dataProvider' => $dataProvider,
+                    'itemView' => '_item',
+                    'layout' => "{items}\n{pager}",
+                    'options' => ['class' => 'news-list'],
+                    'itemOptions' => ['class' => 'news-item'],
+                    'pager' => [
+                        'options' => ['class' => 'news-pager'],
+                        'prevPageLabel' => '<i class="fas fa-chevron-left"></i>',
+                        'nextPageLabel' => '<i class="fas fa-chevron-right"></i>',
+                        'maxButtonCount' => 5,
+                    ]
+                ]); ?>
+            </main>
+        </div>
+    </div>
 </div>
